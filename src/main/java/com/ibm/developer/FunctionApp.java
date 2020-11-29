fb33d180e096e5aacbe0f3e4169467b8394582aa
Billy Korando <wkorando@gmail.com>
2020-11-28 21:38:58 -0600

    Adding code and tekton file

---
+++ src/main/java/com/ibm/developer/FunctionApp.java
@@ -0,0 +1,22 @@
+package com.ibm.developer;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+public class FunctionApp {
+
+	public static JsonObject main(JsonObject args) {
+		JsonObject response = new JsonObject();
+		JsonPrimitive nameArg = args.getAsJsonPrimitive("name");
+		
+		String result;
+		if (nameArg == null) {
+			result = "Hello! Welcome to OpenWhisk...";
+		} else {
+			result = "Hello " + nameArg.getAsString();
+		}
+		response.addProperty("response", result);
+
+		return response;
+	}
+}
